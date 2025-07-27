80f1db36b0a3c4aa1f4d64deea418d9d2366df16
tylertylerday <tylerday89@gmail.com>
2025-07-27 12:21:49 -0700

    Additional carousel option for swiperjs

--- src/FeatureGrid.tsx
+++ src/FeatureGrid.tsx
@@ -1,6 +1,8 @@
+import React, { useState } from 'react'
 import './FeatureGrid.css'
 import homeButton from './assets/homeButton.webp'
 import gridImage1 from './assets/gridImages/gridImage_1.webp'
+import FeatureCarousel from './FeatureCarousel'
 
 interface GridItem {
   title: string
@@ -30,6 +32,26 @@ const customGridItems: GridItem[] = [
 function FeatureGrid({ onNavigateHome, onNavigateToProject, gridItems: overrideGridItems }: FeatureGridProps) {
   // Use override items if provided, otherwise use the customGridItems defined above
   const gridItems = overrideGridItems || customGridItems
+  const [isCarouselView, setIsCarouselView] = useState(false)
+
+  const toggleView = () => {
+    setIsCarouselView(!isCarouselView)
+  }
+
+  if (isCarouselView) {
+    return (
+      <>
+        <button className="carousel-toggle-button" onClick={toggleView}>
+          Grid View
+        </button>
+        <FeatureCarousel 
+          onNavigateHome={onNavigateHome}
+          onNavigateToProject={onNavigateToProject}
+          gridItems={gridItems}
+        />
+      </>
+    )
+  }
 
   const renderedGridItems = gridItems.map((item, index) => (
     <div 
@@ -44,6 +66,9 @@ function FeatureGrid({ onNavigateHome, onNavigateToProject, gridItems: overrideG
 
   return (
     <div className="feature-grid-container">
+      <button className="carousel-toggle-button" onClick={toggleView}>
+        Carousel View
+      </button>
       <div className="feature-grid">
         {renderedGridItems}
       </div>
